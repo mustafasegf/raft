3212ee662e9075a5ec7d00d6c85068d23a78b497
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2022-11-19 22:01:43 +0700

    feat: changed to use engcode length on protobuf

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use prost::Message;
 use raft::message;
-use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::time::{sleep, Duration};
 
@@ -17,7 +17,11 @@ async fn main() {
     };
 
     println!("initial {:?}", &data);
-    println!("initial {:?}", &data.encode_to_vec());
+    println!("initial byte {:?}", &data.encode_to_vec());
+    println!(
+        "initial byte with length {:?}",
+        &data.encode_length_delimited_to_vec()
+    );
 
     let ln = TcpListener::bind("127.0.0.1:0").await.unwrap();
     let addr = ln.local_addr().unwrap();
@@ -25,65 +29,49 @@ async fn main() {
     println!("listening on {}", addr);
 
     let h1 = tokio::spawn(async move {
-        let (stream, addr) = ln.accept().await.unwrap();
+        let (mut stream, addr) = ln.accept().await.unwrap();
         println!("accepted from {}", addr.to_string());
-        let mut stream = BufReader::new(stream);
 
-        let mut buf: Vec<u8> = Vec::with_capacity(1024);
-        loop {
-            tokio::select! {
-                res = stream.read_u8() => {
-                    match res {
-                        Ok(b) => buf.push(b),
-                        Err(e)  if e.kind() == tokio::io::ErrorKind::UnexpectedEof => break,
-                        Err(e) => {
-                            println!("error: {}", e);
-                            break;
-                        },
-                    };
-                },
-                _ = sleep(Duration::from_millis(10)) => break,
-            };
-        }
+        let len = stream.read_u8().await.unwrap();
+        let mut buf: Vec<u8> = vec![0; len as usize];
+        if let Err(err) = stream.read_exact(&mut buf).await {
+            println!("read error: {}", err);
+        };
 
         let req = message::Request::decode(&buf[..]).unwrap();
 
         println!("request {:?}", &req);
 
+        let res = message::Response {
+            term: 1,
+            status: true,
+        };
         match req.requests {
-            Some(message::request::Requests::Vote(..)) => {
-                println!("vote request");
-                let res = message::Response {
-                    term: 1,
-                    status: true,
-                };
-
-                stream.write_all(&res.encode_to_vec()).await.unwrap();
-            }
-            Some(message::request::Requests::Append(..)) => {
-                println!("append request");
-                let res = message::Response {
-                    term: 1,
-                    status: true,
-                };
-
-                stream.write_all(&res.encode_to_vec()).await.unwrap();
-            }
+            Some(message::request::Requests::Vote(..)) => println!("vote request"),
+            Some(message::request::Requests::Append(..)) => println!("append request"),
             None => println!("error: no request"),
         };
+
+        stream
+            .write_all(&res.encode_length_delimited_to_vec())
+            .await
+            .unwrap();
         println!("from server {:?}", &req);
     });
 
     let h2 = tokio::spawn(async move {
         let mut stream = TcpStream::connect(&addr).await.unwrap();
-        stream.write_all(&data.encode_to_vec()).await.unwrap();
+        stream
+            .write_all(&data.encode_length_delimited_to_vec())
+            .await
+            .unwrap();
 
         let mut buf: Vec<u8> = Vec::with_capacity(1024);
 
         if let Err(err) = stream.read_to_end(&mut buf).await {
             println!("error {:?}", err);
         };
-        let req = message::Response::decode(&buf[..]).unwrap();
+        let req = message::Response::decode(&buf[1..]).unwrap();
         println!("response {:?}", &req);
     });
 
